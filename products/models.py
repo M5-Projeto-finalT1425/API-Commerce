e9a749909b79ca1981a661e5d668b438a54090f2
joaobuga35 <joaobuga35@gmail.com>
2023-05-03 21:12:31 -0300

    feat: create model and serializer file on products

--- products/models.py
+++ products/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from accounts.models import Account
 
 
 class Product(models.Model):
-    ...
+    class Meta:
+        ordering = ["id"]
+
+    name = models.CharField(max_length=150)
+    quantity = models.IntegerField(default=0)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name="products",
+        null=True,
+        blank=True,
+    )
