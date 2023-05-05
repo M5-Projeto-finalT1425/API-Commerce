31d04af457aa28ce56b5cced9346c64da5aec9b7
LuccaHaddadSerejo <hslucca@gmail.com>
2023-05-05 07:11:34 -0300

    feat: returning full object in products

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Cart
 from django.shortcuts import get_object_or_404
+from products.serializers import ProductSerializer
 
 
 class CartSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
     def update(self, instance, validated_data):
         user = validated_data.pop("user")
         cart = get_object_or_404(Cart, id=user.cart.id)
