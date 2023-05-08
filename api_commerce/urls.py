433818073b6c942699ab91d3676bd22d5abb6361
LuccaHaddadSerejo <hslucca@gmail.com>
2023-05-08 17:28:33 -0300

    refactor: Changing core application name

--- api_commerce/urls.py
+++ api_commerce/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for _APICommerce project.
+URL configuration for api_commerce project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- api_commerce/wsgi.py
+++ api_commerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for _APICommerce project.
+WSGI config for api_commerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_APICommerce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_commerce.settings")
 
 application = get_wsgi_application()
